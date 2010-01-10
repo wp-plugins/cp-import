48e0faa21f2a47da8e861eec1355ed1e208533a8
johnl1479 <johnl1479@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-10 19:29:45 +0000

    afinished settings
    git-svn-id: https://plugins.svn.wordpress.org/cp-import/trunk@192322 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/cp-import-settings.class.php
+++ classes/cp-import-settings.class.php
@@ -27,6 +27,7 @@ class CP_Import_Settings {
 
 	// THIS version of CP Import
 	var $version = "2.0";
+	var $errors = array();
 
 	public function CP_Import_Settings () {
 		$this->options = $this->get();
@@ -35,17 +36,18 @@ class CP_Import_Settings {
 	public function upgrade() {
 		
 		$options[] = new CP_Import_Option('from_version', 4, 'Which version of College Publisher are you importing from?', true, 'radio', array ('4' => 'CP 4')); //, '5' => 'CP 5'));
-		$options[] = new CP_Import_Option('users', 'accounts', 'CP Import can either create a Wordpress User account for each author that it finds, or add that information to each post as a custom field.<br /><br />More options are below if you choose "Wordpress Account". If you choose "Custom Field", author data will be imported as-is from you Archive file.', true, 'radio', array('accounts' => 'Wodpress Account', 'custom_field' => 'Custom Field', 'none' => 'None'));
+		$options[] = new CP_Import_Option('users', 'accounts', 'CP Import can either create a Wordpress User account for each author that it finds, or add that information to each post as a custom field.<br /><br />More options are below if you choose "Wordpress Account". If you choose "Custom Field", author data will be imported as-is from you Archive file.', true, 'radio', array('accounts' => 'Wordpress Account', 'custom_field' => 'Custom Field', 'none' => 'None'));
 		$options[] = new CP_Import_Option('default_user', 1, 'If any kind of error occurs while attempting to import author data, or you chose to import article authors as Custom Fields, which account should have credit for having written the imported articles?', true, 'select', get_users_of_blog());
 		$options[] = new CP_Import_Option('uname_prefix', '', 'If you choose to create Wordpress Accounts for each author, CP Import automatically names the account <em>firstname.lastname</em>. However, you can prefix this with whatever you want', true);
 		$options[] = new CP_Import_Option('uname_suffix', '', 'You can also add a suffix to the generated username.', true);
 		$options[] = new CP_Import_Option('verbose', 0, 'Would you like additional output displayed when importing articles?', false, 'radio', array('1' => 'Yes', '0' => 'No'));
 		$options[] = new CP_Import_Option('date_fmt', 'Y-m-d H:i:s', 'How should dates be formatted in the database? (PHP date() syntax)');
-		$options[] = new CP_Import_Option('cp4url', '"/media/storage/paper%s/news/%year%/%monthnum%/%day%/%category%/%postname%-%post_id%.shtml');
+		$options[] = new CP_Import_Option('cp4url', '"/media/storage/paper%paper_id%/news/%year%/%monthnum%/%day%/%category%/%postname%-%post_id%.shtml');
 		$options[] = new CP_Import_Option('cp5url', '/%category%/%postname%-1.%post_id%"');
+		$options[] = new CP_Import_Option('permalink_string', '');
 		$options[] = new CP_Import_Option('cp_permalinks', '0', 'CP Import can also set your Wordpress permalinks to mimic that of College Publisher. If you turn this on, please remeber to enter your College Publisher Paper ID below.', true, 'radio', array('1' => 'Yes', '0' => 'No'));
 		$options[] = new CP_Import_Option('paper_id', '', 'Your College Publisher Paper ID (Required if you choose to mimic CP Permalinks above)', true);
-		$options[] = new CP_Import_Option('redirect', '1', 'If you do not want to set your permalinks to mimic that of College Publisher, CP Import can attempt to redirect your old College Publisher links to your new Wordpress ones. (Special Thanks to <a href="http://danielbachhuber.com/">Daniel Bachhuber</a> of <a href="http://copress.org/">CoPress</a> for this)', true, 'radio', array( '1' => 'Enable', '0' => 'Disable'));
+		$options[] = new CP_Import_Option('cp_redirect', '1', 'If you do not want to set your permalinks to mimic that of College Publisher, CP Import can attempt to redirect your old College Publisher links to your new Wordpress ones. (Based on <a href="http://wordpress.org/extend/plugins/cp-redirect/">CP Redirect</a> written by <a href="http://danielbachhuber.com/">Daniel Bachhuber</a> of <a href="http://copress.org/">CoPress</a>)', true, 'radio', array( '1' => 'Enable', '0' => 'Disable'));
 		$options[] = new CP_Import_Option('media_dir', WP_CONTENT_DIR."/cp-import/");
 		$options[] = new CP_Import_Option('media_dir_hr', basename(dirname(WP_CONTENT_DIR."/cp-import/"))."/" . basename(WP_CONTENT_DIR."/cp-import/")."/");
 		$options[] = new CP_Import_Option('temp_dir', plugin_dir_path(__FILE__)."tmp/");
@@ -101,13 +103,69 @@ class CP_Import_Settings {
 		update_option(CP_IMPORT_DB_OPTIONS, $this->options);
 	}
 
-	public function ui_error($msg) {
+	public function log_error($msg) {
+		$this->errors[] = $msg;
+	}
 
+	public function errors() {
+		$r = "";
+		foreach ($this->errors as $e) {
+			$r .= "<div class='error'><p><strong>Error:</strong> ".$e."</p></div>";
+		}
+
+
+		if (sizeof($this->errors) > 0) {
+			echo $r; return true;
+		}		
+		else 
+			return false;
 	}
 	
+	public function ui_success($msg) {
+		echo "<div class='updated'><p>".$msg."</p></div>";
+	}
+
 	public function ui_process_post() {
-		print_r($_POST);
-		
+		// First, do requirement checks.
+		// 1) If permalinks are enabled, Paper ID is required.
+		if (($_REQUEST['cp_permalinks'] != 0) && (empty($_REQUEST['paper_id']))) {
+			$this->log_error('You musr enter your Paper ID when enabling CP-style permalinks');
+		}
+		// 2) Pernalinks and redirection cannot be enabled at the same time.
+		if (($_REQUEST['cp_permalinks'] == 1) && ($_REQUEST['cp_redirect'] == 1)) {
+			$this->log_error('CP Permalinks and Redirect cannot be enabled at the same time.');
+		}
+
+		if(!$this->errors()) {
+			// There were now errors, so now, looop through $_REQUEST, if an index matches
+			// an option name, update the option with the new value
+			foreach ($_REQUEST as $k=>$v) {
+				foreach ($this->options as $opt) {
+					if ($opt->getName() == $k) {
+						$opt->setValue($v);
+					}
+				}
+			}
+
+			$this->save();
+
+			// The permalink string must be manually set
+			if ($this->get('from_version')->getValue() == '4') {
+				$this->set(
+					'permalink_string',
+					str_replace("%paper_id%", $this->get('paper_id')->getValue(), $this->get('cp4url')->getValue())
+				);
+			}
+			else { // from version 5
+				$this->set(
+					'permalink_string',
+					$this->get('cp4url')->getValue()
+				);
+			}
+
+			echo $this->ui_success("Options saved!");
+		}
+
 	}
 
 	public function ui_screen() {
