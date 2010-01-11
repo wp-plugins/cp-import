6a7181679cf97513537ae16a2e50f6f7c53d9013
johnl1479 <johnl1479@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-11 20:42:42 +0000

    - updated svn:ignore - updated license - added filesplitter class - added writable directory for the filesplitter class to work in
    git-svn-id: https://plugins.svn.wordpress.org/cp-import/trunk@192844 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/cp-import-filesplitter.class.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * CP Import
+ *
+ * @package net.johnluetke.software.wordpress.cp-import
+ *
+ * @copyright 2009 John Luetke < john@johnluetke.net >
+ *
+ * $URL$
+ * $Revision$
+ * $Date$
+ * $Author$
+ *
+ * @filesource
+ */
+
+class CP_Import_FileSplitter{
+
+	var $_source = '';
+	var $_lines = 0;
+	var $_path = '';
+
+	function configure ( $source = "", $path = "", $lines = "") {
+		if ($source != "") $this->_source = $source;
+		if ($path != "") $this->_path = $path;
+		if ($lines != "") $this->_lines = $lines;
+	}
+
+	function run () {
+		$i = 0;
+		$j = 1;
+
+		$date = date("Ymd");
+		$handle = @fopen ($this->_source, "r");
+		$buffer = "";
+
+		while (!feof ($handle)) {
+			$buffer .= @fgets($handle);
+			$i++;
+			
+			if ($i >= $this->_lines) {
+				$fname = $this->_path . "part-".$date."-".$j.".txt";
+
+				if (!$fhandle = @fopen($fname, 'w')) die("Cannot open file $fname");
+				if (!@fwrite($fhandle, $buffer)) die("Cannot write to file $fname");
+			}
+
+			fclose($fhandle);
+			$j++;
+
+			unset($buffer, $i);
+		}
+
+		fclose ($handle);
+	}
+}
+?>
